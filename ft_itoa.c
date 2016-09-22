e9098a7097a1f3e848fda45403093cb38b540306
Jack Halford <jack@crans.org>
2016-09-22 14:07:20 +0200

    fixed errors with macro FT_ prefix

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,25 +1,5 @@
 #include "libft.h"
 
-static char		*ft_strrev(char *str)
-{
-	int		len;
-	char	tmp;
-	int		i;
-
-	i = 0;
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	while (i < len / 2)
-	{
-		tmp = str[len - (i + 1)];
-		str[len - (i + 1)] = str[i];
-		str[i] = tmp;
-		i++;
-	}
-	return (str);
-}
-
 static size_t	ft_size(int n)
 {
 	size_t	i;
@@ -45,10 +25,10 @@ char	*ft_itoa(int n)
 	}
 	while (n)
 	{
-		str[i++] = ABS(n % 10) + '0';
+		str[i++] = FT_ABS(n % 10) + '0';
 		n /= 10;
 	}
-	if (NEG(n))
+	if (FT_NEG(n))
 		str[i++] = '-';
 	str[i] = '\0';
 	return (ft_strrev(str));
