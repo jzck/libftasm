af71e8b8de26051cd63fde8ab82c90801bd835d8
Jack Halford <jack@crans.org>
2017-01-10 12:34:08 +0100

    lstfind more explicit NULL

--- src/lst/ft_lst_find.c
+++ src/lst/ft_lst_find.c
@@ -6,7 +6,7 @@
 /*   By: jhalford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 11:09:20 by jhalford          #+#    #+#             */
-/*   Updated: 2016/12/12 12:54:45 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/01/10 11:07:37 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ t_list	*ft_lst_find(t_list *begin_list, void *data_ref, int (*cmp)())
 			return (list_ptr);
 		list_ptr = list_ptr->next;
 	}
-	return (list_ptr);
+	return (NULL);
 }
