d79c38104bbd45018d03a11d0c60cd6616e77ed8
Jack Halford <jack@crans.org>
2017-02-07 20:12:51 +0100

    replaced escape codes with \e

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jhalford <jack@crans.org>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/02/07 16:09:36 by jhalford          #+#    #+#              #
-#    Updated: 2017/02/07 16:13:25 by jhalford         ###   ########.fr        #
+#    Updated: 2017/02/07 20:11:46 by jhalford         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -194,14 +194,14 @@ all: $(NAME)
 $(NAME): $(OBJ_DIR) $(OBJS)
 	@$(AR) $(NAME) $(OBJS)
 	@ranlib $(NAME)
-	@echo "\r\033[48;5;15;38;5;25m✅ MAKE $(NAME)\033[0m\033[K"
+	@printf "\r\e[48;5;15;38;5;25m✅ MAKE $(NAME)\e[0m\e[K\n"
 
 $(OBJ_DIR)%.o: $(SRC_DIR)%.c | $(OBJ_DIR)
 	@$(eval DONE=$(shell echo $$(($(INDEX)*20/$(NB)))))
 	@$(eval PERCENT=$(shell echo $$(($(INDEX)*100/$(NB)))))
 	@$(eval COLOR=$(shell echo $$(($(PERCENT)%35+196))))
 	@$(eval TO_DO=$(shell echo $$((20-$(INDEX)*20/$(NB)))))
-	@printf "\r\033[38;5;11m⌛ MAKE %10.10s : %2d%% \033[48;5;%dm%*s\033[0m%*s\033[48;5;255m \033[0m \033[38;5;11m %*s\033[0m\033[K" $(NAME) $(PERCENT) $(COLOR) $(DONE) "" $(TO_DO) "" $(DELTA) "$@"
+	@printf "\r\e[38;5;11m⌛ MAKE %10.10s : %2d%% \e[48;5;%dm%*s\e[0m%*s\e[48;5;255m \e[0m \e[38;5;11m %*s\e[0m\e[K" $(NAME) $(PERCENT) $(COLOR) $(DONE) "" $(TO_DO) "" $(DELTA) "$@"
 	@$(MKDIR) $(OBJ_DIR)
 	@$(CC) $(FLAGS) $(D_FLAGS) -MMD -c $< -o $@\
 		-I $(INC_DIR)
@@ -213,11 +213,11 @@ $(OBJ_DIR):
 
 clean:
 	@$(RM) $(OBJ_DIR)
-	@echo "\r\033[38;5;202m✖ clean $(NAME).\033[0m\033[K"
+	@printf "\r\e[38;5;202m✖ clean $(NAME).\e[0m\e[K\n"
 
 fclean: clean
 	@$(RM) $(NAME)
-	@echo "\r\033[38;5;196m❌ fclean $(NAME).\033[0m\033[K"
+	@printf "\r\e[38;5;196m❌ fclean $(NAME).\e[0m\e[K\n"
 
 re: fclean all
 
--- includes/color.h
+++ includes/color.h
@@ -6,7 +6,7 @@
 /*   By: jhalford <jack@crans.org>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 13:36:48 by jhalford          #+#    #+#             */
-/*   Updated: 2016/12/09 22:09:24 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/02/07 20:09:07 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,30 @@ struct	s_color
 
 typedef struct s_color	t_color;
 
-# define FG_DEFAULT		"\x1b[0m"
-# define ON_BOLD		"\x1b[1m"
+# define FG_DEFAULT		"\e[0m"
+# define ON_BOLD		"\e[1m"
 
-# define ON_UNDERLINED	"\x1b[4m"
-# define ON_INVERTED	"\x1b[7m"
+# define ON_UNDERLINED	"\e[4m"
+# define ON_INVERTED	"\e[7m"
 
-# define FG_BLACK		"\x1b[30m"
-# define FG_RED			"\x1b[31m"
-# define FG_GREEN		"\x1b[32m"
-# define FG_YELLOW		"\x1b[33m"
-# define FG_BLUE		"\x1b[34m"
-# define FG_MAGENTA		"\x1b[35m"
-# define FG_CYAN		"\x1b[36m"
+# define FG_BLACK		"\e[30m"
+# define FG_RED			"\e[31m"
+# define FG_GREEN		"\e[32m"
+# define FG_YELLOW		"\e[33m"
+# define FG_BLUE		"\e[34m"
+# define FG_MAGENTA		"\e[35m"
+# define FG_CYAN		"\e[36m"
 
-# define BG_BLACK		"\x1b[40m"
-# define BG_RED			"\x1b[41m"
-# define BG_GREEN		"\x1b[42m"
-# define BG_YELLOW		"\x1b[43m"
-# define BG_BLUE		"\x1b[44m"
-# define BG_MAGENTA		"\x1b[45m"
-# define BG_CYAN		"\x1b[46m"
-# define BG_DEFAULT		"\x1b[49m"
+# define BG_BLACK		"\e[40m"
+# define BG_RED			"\e[41m"
+# define BG_GREEN		"\e[42m"
+# define BG_YELLOW		"\e[43m"
+# define BG_BLUE		"\e[44m"
+# define BG_MAGENTA		"\e[45m"
+# define BG_CYAN		"\e[46m"
+# define BG_DEFAULT		"\e[49m"
 
-# define FBG_DEFAULT	"\x1b[49m\x1b[20m"
+# define FBG_DEFAULT	"\e[49m\e[20m"
 
 void	ft_color_reset(void);
 void	ft_color_set(t_color color);
