9aa3049dd23424807316a6f027589a95cdf1fcdd
Jack Halford <jack@crans.org>
2017-02-20 18:19:59 +0100

    put_endl_fd, deso deso wescande

--- src/printing/ft_putendl.c
+++ src/printing/ft_putendl.c
@@ -6,17 +6,22 @@
 /*   By: jhalford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 14:57:40 by jhalford          #+#    #+#             */
-/*   Updated: 2016/11/03 14:57:41 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/02/20 18:17:01 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl(char const *s)
+void	ft_putendl_fd(char const *s, int fd)
 {
 	char	nl;
 
 	nl = '\n';
-	write(1, s, ft_strlen(s));
-	write(1, &nl, 1);
+	write(fd, s, ft_strlen(s));
+	write(fd, &nl, 1);
+}
+
+void	ft_putendl(char const *s)
+{
+	ft_putendl_fd(s, 1);
 }
