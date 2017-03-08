471b7194b7c6e56e3911ff49c9f2f7632fdc4b98
Jack Halford <jack@crans.org>
2017-03-08 11:57:52 +0100

    lstmap change like btree_map

--- src/lst/ft_lstmap.c
+++ src/lst/ft_lstmap.c
@@ -6,24 +6,19 @@
 /*   By: jhalford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/03 14:57:21 by jhalford          #+#    #+#             */
-/*   Updated: 2017/03/02 17:48:58 by jhalford         ###   ########.fr       */
+/*   Updated: 2017/03/08 11:57:03 by jhalford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *))
 {
 	t_list	*out;
 	t_list	*elem;
 
-	out = NULL;
-	while (lst)
-	{
-		elem = (*f)(lst);
-		elem = ft_lstnew(elem->content, elem->content_size);
-		ft_lsteadd(&out, elem);
-		lst = lst->next;
-	}
-	return (out);
+	elem = ft_lstnew(lst->content, lst->content_size);
+	elem = (*f)(lst);
+	elem->next = ft_lstmap(lst->next, f);
+	return (elem);
 }
